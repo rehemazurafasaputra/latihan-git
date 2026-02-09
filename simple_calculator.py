25603c0f83d1de13e13417d8b22ac27243e35ac8
Rehema Zurafa Saputra <rehemazurafasaputra@gmail.com>
2026-02-09 16:36:24 +0700

    initial commit.

---
+++ simple_calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+ def add(self, a, b):
+    return a + b
+
+ def subtract(self, a, b):
+    return a - b
+
+ def multiply(self, a, b):
+    return a * b
+ 
+if __name__ == "__main__":
+ calc = Calculator()
+ print("Addition: ", calc.add(10, 5))
+ print("Subtraction: ", calc.subtract(10, 5))
+ print("Multiplication: ", calc.multiply(10, 5))
